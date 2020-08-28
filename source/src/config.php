d4a3bfb0f49b6cb2b19e6696318c76e23f4759aa
Cassio Amorim <amorim.cassio@chino-js.com>
2020-08-28 19:51:59 +0900

    DB fix

--- source/src/config.php
+++ source/src/config.php
@@ -3,10 +3,10 @@ define('LIBRARY', 'scigen/lib/');
 define('BASE_URL', 'http://localhost/');
 
 define('DB_VENDOR', 'mysql');
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db');
 define('DB_PORT', '3306');
-define('DB_NAME', 'database');
+define('DB_NAME', 'PeerRep');
 define('DB_USER', 'root');
-define('DB_PWD', 'top-secret');
+define('DB_PWD', 'topsecret');
 
 ?>
